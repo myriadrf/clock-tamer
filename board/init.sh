3cbf20bfcf5e1c6fbb92617c7333536cf27a821f
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2009-12-26 00:01:19 +0300

    More intelligent init.sh

--- board/init.sh
+++ board/init.sh
@@ -1,20 +1,56 @@
 #!/bin/sh
 
-echo "RST" >/dev/ttyACM0 
-sleep 1
-echo "SET,VCO,MIN,1590" > /dev/ttyACM0 
-sleep 1
-echo "SET,VCO,MAX,1700" > /dev/ttyACM0 
-sleep 1
-echo "SET,VCO,KBT,2100" > /dev/ttyACM0 
-sleep 1
-echo "SET,,OSC,26000000" > /dev/ttyACM0 
-sleep 1
-echo "SET,,OUT,52000000" > /dev/ttyACM0 
-sleep 1
-echo "SET,LMK,PRT,64" > /dev/ttyACM0 
-sleep 1
-echo "SET,,AUT,1" > /dev/ttyACM0 
-sleep 1
-echo "STE" >/dev/ttyACM0 
+TAMER_DEV="ttyACM0"
+TAMER_VCO="2080"
 
+tamer_echo()
+{
+    echo $* >>$TAMER_DEV
+    sleep 0.2
+}
+
+tamer_read()
+{
+    head -n 1 $TAMER_DEV
+}
+
+tamer_exec()
+{
+    echo "<- $1"
+    tamer_echo $1
+    tamer_tmp=$(tamer_read)
+    echo "-> $tamer_tmp"
+    if test -n "$2" -a "$tamer_tmp" != "$2" ; then
+        echo "*** ERROR ***"
+        echo "Reply is not equal to \"$2\""
+        exit 1
+    fi
+}
+
+set_vco()
+{
+case "$1" in
+1515)
+    tamer_exec "SET,VCO,MIN,1450" "OK"
+    tamer_exec "SET,VCO,MAX,1580" "OK"
+    tamer_exec "SET,VCO,KBT,2000" "OK"
+    ;;
+2080)
+    tamer_exec "SET,VCO,MIN,1904" "OK"
+    tamer_exec "SET,VCO,MAX,2274" "OK"
+    tamer_exec "SET,VCO,KBT,4500" "OK"
+    ;;
+*)
+    tamer_exec "Unknown VCO: \"$1\". Aborting."
+    exit 1
+    ;;
+esac
+}
+
+tamer_exec "RST" "OK"
+set_vco $TAMER_VCO
+tamer_exec "SET,,OSC,20000000"
+tamer_exec "SET,,OUT,52000000" "OK"
+tamer_exec "SET,LMK,PRT,64" "OK"
+tamer_exec "SET,,AUT,1" "OK"
+tamer_exec "STE" "OK"
